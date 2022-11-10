08616cbeb0ade273591630ae6b4893a843a9df33
Egor Shubin <egor.shubin@bk.ru>
2022-11-10 16:35:17 +0100

    send Email

--- CouncilProcess.py
+++ CouncilProcess.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from datetime import datetime
 import time
+from Email import Email
 
 
 class CouncilProcess:
@@ -15,7 +16,8 @@ class CouncilProcess:
         self.driver.get(self.url)
 
         image = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//*[@alt="Необходимо включить загрузку картинок в браузере."]'))
+        base64 = image.screenshot_as_base64
+        email = Email(self.url)
+        email.send()
 
-        print(image.screenshot_as_base64)
-
-        time.sleep(5)
+        time.sleep(3)
--- Email.py
+++ Email.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
+import smtplib
+from email.message import EmailMessage
+
 _ = load_dotenv()
+
 
 class Email:
     def __init__(self, url):
@@ -9,3 +13,25 @@ class Email:
         self.email_to = os.environ.get("EMAIL_TO")
         self.url = url
 
+    def send(self):
+        try:
+            if self.email_from is None or self.app_password is None:
+                print("Did you set email-from and password correctly?")
+                return False
+
+            # create email
+            msg = EmailMessage()
+            msg['Subject'] = 'Successful council'
+            msg['From'] = self.email_from
+            msg['To'] = self.email_to
+            msg.set_content(self.url)
+
+            # send email
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(self.email_from, self.app_password)
+                smtp.send_message(msg)
+            return True
+        except Exception as e:
+            print("Problem during send email")
+            print(str(e))
+        return False
